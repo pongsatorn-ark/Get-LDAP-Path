80790d5deeb3ac3cf2f12048126afd23b2ad2468
Pongsatorn <58293089+pongsatorn-ark@users.noreply.github.com>
2025-02-26 21:23:38 +0700

    Update ldap_path.ps1

--- ldap_path.ps1
+++ ldap_path.ps1
@@ -1,4 +1,15 @@
-$PDC = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name
-$DN = ([adsi]'').distinguishedName 
-$LDAP = "LDAP://$PDC/$DN"
-$LDAP
+function LDAPSearch {
+    param (
+        [string]$LDAPQuery
+    )
+
+    $PDC = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name
+    $DistinguishedName = ([adsi]'').distinguishedName
+
+    $DirectoryEntry = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$PDC/$DistinguishedName")
+
+    $DirectorySearcher = New-Object System.DirectoryServices.DirectorySearcher($DirectoryEntry, $LDAPQuery)
+
+    return $DirectorySearcher.FindAll()
+
+}
